84f71c4be66122078155d1c84d4c2b0c95d5a148
blchnk <bulycevn@gmail.com>
2024-10-13 17:27:22 +0300

    init bot

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func main() {
+	bot, err := InitBot()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ListenUpdates(bot)
+}
+
+func InitBot() (*tgbotapi.BotAPI, error) {
+	token := os.Getenv("API_KEY")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	return bot, nil
+}
+
+func ListenUpdates(bot *tgbotapi.BotAPI) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
